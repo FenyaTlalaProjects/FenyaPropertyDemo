be6a7d75901f68bc0da160f08f0a12c2b79fcd73
Kingzp <zpthile@gmail.com>
2018-07-24 15:51:46 +0200

    Project Initial commit

---
+++ src/main/java/za/co/fenyatlala/FTPS_Demo/FTPS_Demowe/FtpsDemoweApplication.java
@@ -0,0 +1,12 @@
+package za.co.fenyatlala.FTPS_Demo.FTPS_Demowe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtpsDemoweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtpsDemoweApplication.class, args);
+	}
+}
---
+++ src/test/java/za/co/fenyatlala/FTPS_Demo/FTPS_Demowe/FtpsDemoweApplicationTests.java
@@ -0,0 +1,16 @@
+package za.co.fenyatlala.FTPS_Demo.FTPS_Demowe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FtpsDemoweApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
